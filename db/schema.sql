a62e2d507033b0fa9f97aff5d8416a73dfb08553
Binh <kbinhnguyen.nihongo@gmail.com>
2022-07-28 01:35:53 -0400

    Seeding completed. Kept same schema as legacy db

--- db/schema.sql
+++ db/schema.sql
@@ -2,43 +2,49 @@
 To run this file, in the command line: run `psql postgres -f ./db/schema.sql`
 */
 
-DROP DATABASE mydb;
+DROP DATABASE IF EXISTS mydb;
 CREATE DATABASE mydb;
 
 \c mydb;
 
-CREATE TABLE IF NOT EXISTS characteristics (
-  id smallserial primary key,
-  characteristic varchar(255) not null
-);
-
-CREATE TABLE IF NOT EXISTS review (
+CREATE TABLE IF NOT EXISTS reviews (
   id bigserial primary key,
+  product_id bigint not null,
   rating integer not null,
-  CHECK (rating < 6 AND rating < -1),
-  summary varchar(60) not null,
+  CHECK (rating < 6 AND rating > -1),
+  date bigint not null,
+  summary varchar(255) not null,
   body text not null,
-  CHECK (char_length(body) < 1001 AND char_length(body) > 49),
-  reviewer_name varchar(255) not null,
-  email varchar(255) not null,
-  reported boolean not null DEFAULT false,
-  response text,
-  helpfulness integer not null DEFAULT 0,
-  date_created date not null,
+  -- CHECK (char_length(body) < 1001 AND char_length(body) > 49),
   recommended boolean not null DEFAULT false,
-  product_id integer not null
+  reported boolean not null DEFAULT false,
+  reviewer_name varchar(255) not null,
+  reviewer_email varchar(255) not null,
+  response text,
+  helpfulness integer not null DEFAULT 0
 );
 
-CREATE TABLE IF NOT EXISTS photo (
+CREATE TABLE IF NOT EXISTS photos (
   id bigserial primary key,
-  url varchar(255) not null,
-  review_id integer not null references review(id)
+  review_id bigint not null references reviews(id),
+  url text not null
 );
 
-CREATE TABLE IF NOT EXISTS characteristics_review (
+CREATE TABLE characteristics (
   id bigserial primary key,
-  review_id integer not null references review(id),
-  characteristic_id integer not null references characteristics(id),
-  characteristic_rating integer not null,
-  CHECK (characteristic_rating < 6 AND characteristic_rating < -1)
+  product_id bigint not null,
+  name varchar(50) not null
 );
+
+CREATE TABLE IF NOT EXISTS characteristic_reviews (
+  id bigserial primary key,
+  characteristic_id bigint not null references characteristics(id),
+  review_id bigint not null references reviews(id),
+  value integer not null,
+  CHECK (value < 6 AND value > -1)
+);
+
+\copy reviews (id, product_id, rating, date, summary, body, recommended, reported, reviewer_name, reviewer_email, response, helpfulness) FROM './db/rawdata/reviews.csv' WITH (FORMAT csv, HEADER true);
+\copy characteristics (id, product_id, name) FROM './db/rawdata/characteristics.csv' WITH (FORMAT csv, HEADER true);
+\copy photos (id, review_id, url) FROM './db/rawdata/reviews_photos.csv' WITH (FORMAT csv, HEADER true);
+\copy characteristic_reviews (id, characteristic_id, review_id, value) FROM './db/rawdata/characteristic_reviews.csv' WITH (FORMAT csv, HEADER true);
