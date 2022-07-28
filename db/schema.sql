5b6650cf46650a8a8c4fdbe1a7b0752e48f10e44
Binh <kbinhnguyen.nihongo@gmail.com>
2022-07-28 15:39:07 -0400

    Reset id column values to keep auto_incrementing from the correct value

--- db/schema.sql
+++ db/schema.sql
@@ -44,3 +44,10 @@ CREATE TABLE IF NOT EXISTS characteristic_reviews (
 \copy characteristics (id, product_id, name) FROM './db/rawdata/characteristics.csv' WITH (FORMAT csv, HEADER true);
 \copy photos (id, review_id, url) FROM './db/rawdata/reviews_photos.csv' WITH (FORMAT csv, HEADER true);
 \copy characteristic_reviews (id, characteristic_id, review_id, value) FROM './db/rawdata/characteristic_reviews.csv' WITH (FORMAT csv, HEADER true);
+
+-- ALTER TABLE reviews ALTER COLUMN date TYPE timestamp USING TO_TIMESTAMP(date / 1000) AT TIMEZONE 'utc';
+
+SELECT setval(pg_get_serial_sequence('characteristics', 'id'), coalesce(max(id)+1, 1), false) FROM characteristics;
+SELECT setval(pg_get_serial_sequence('characteristic_reviews', 'id'), coalesce(max(id)+1, 1), false) FROM characteristic_reviews;
+SELECT setval(pg_get_serial_sequence('photos', 'id'), coalesce(max(id)+1, 1), false) FROM photos;
+SELECT setval(pg_get_serial_sequence('reviews', 'id'), coalesce(max(id)+1, 1), false) FROM reviews;
