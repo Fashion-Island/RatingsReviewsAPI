397304473e3a1eabac099ec6528848ce678cd32b
Binh <kbinhnguyen.nihongo@gmail.com>
2022-08-03 10:58:02 -0400

    Added static verification file for loader.io

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('server/static'));
 app.use('/reviews', router);
 
 app.listen(process.env.SERVER_PORT, () => {
